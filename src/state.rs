0ed7c2c194a4e5a159b78646c09413a207eb94b0
Steven Chang <stevenchang5000@protonmail.com>
2021-05-10 13:32:51 +1000

    Store failure if locker name already exists (#4)
    Co-authored-by: Steven Chang <stevenchang5000@gmail.com>

--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,8 @@
 use cosmwasm_std::{ReadonlyStorage, StdResult, Storage};
-use cosmwasm_storage::{PrefixedStorage, ReadonlyPrefixedStorage};
+use cosmwasm_storage::PrefixedStorage;
 use schemars::JsonSchema;
 use secret_toolkit::serialization::{Bincode2, Serde};
+use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 
 // === STORAGE PREFIXES ===
@@ -25,35 +26,30 @@ impl<'a, S: Storage> LockersStorage<'a, S> {
         }
     }
 
+    fn as_readonly(&self) -> ReadonlyLockersStorageImpl<PrefixedStorage<S>> {
+        ReadonlyLockersStorageImpl(&self.storage)
+    }
+
+    pub fn get_locker(&self, key: &String) -> Option<Locker> {
+        self.as_readonly().get(key)
+    }
+
     pub fn set_locker(&mut self, key: &[u8], value: Locker) {
         save(&mut self.storage, &key, &value).ok();
     }
 }
 
-pub struct ReadonlyLockersStorage<'a, S: ReadonlyStorage> {
-    storage: ReadonlyPrefixedStorage<'a, S>,
-}
-
-impl<'a, S: ReadonlyStorage> ReadonlyLockersStorage<'a, S> {
-    pub fn from_storage(storage: &'a S) -> Self {
-        Self {
-            storage: ReadonlyPrefixedStorage::new(PREFIX_LOCKERS, storage),
-        }
-    }
-
-    fn as_readonly(&self) -> ReadonlyLockersStorageImpl<ReadonlyPrefixedStorage<S>> {
-        ReadonlyLockersStorageImpl(&self.storage)
-    }
-}
-
-/// This struct refactors out the readonly methods that we need for `Balances` and `ReadonlyBalances`
-/// in a way that is generic over their mutability.
-///
-/// This was the only way to prevent code duplication of these methods because of the way
+// This struct refactors out the readonly methods in a way that is generic over their mutability.
+// This was the only way to prevent code duplication of these methods because of the way
 /// that `ReadonlyPrefixedStorage` and `PrefixedStorage` are implemented in `cosmwasm-std`
 struct ReadonlyLockersStorageImpl<'a, S: ReadonlyStorage>(&'a S);
 
-impl<'a, S: ReadonlyStorage> ReadonlyLockersStorageImpl<'a, S> {}
+impl<'a, S: ReadonlyStorage> ReadonlyLockersStorageImpl<'a, S> {
+    pub fn get(&self, key: &String) -> Option<Locker> {
+        let alias: Option<Locker> = may_load(self.0, &key.as_bytes()).ok().unwrap();
+        alias
+    }
+}
 
 // === FUNCTIONS ===
 
@@ -62,7 +58,17 @@ impl<'a, S: ReadonlyStorage> ReadonlyLockersStorageImpl<'a, S> {}
 // storage - a mutable reference to the storage this item should go to
 // key - a byte slice representing the key to access the stored item
 // value - a reference to the item to store
-pub fn save<T: Serialize, S: Storage>(storage: &mut S, key: &[u8], value: &T) -> StdResult<()> {
+fn save<T: Serialize, S: Storage>(storage: &mut S, key: &[u8], value: &T) -> StdResult<()> {
     storage.set(key, &Bincode2::serialize(value)?);
     Ok(())
+}
+
+fn may_load<T: DeserializeOwned, S: ReadonlyStorage>(
+    storage: &S,
+    key: &[u8],
+) -> StdResult<Option<T>> {
+    match storage.get(key) {
+        Some(value) => Bincode2::deserialize(&value).map(Some),
+        None => Ok(None),
+    }
 }
