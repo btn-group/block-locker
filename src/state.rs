9b92ad100a8e996927954d9b4de7dc8fd86c35cf
Steven Chang <stevenchang5000@protonmail.com>
2021-05-10 11:14:16 +1000

    Strip back and setup basic framework (#2)
    Co-authored-by: Steven Chang <stevenchang5000@gmail.com>

--- src/state.rs
+++ src/state.rs
@@ -1,21 +1,51 @@
+use cosmwasm_std::{ReadonlyStorage, Storage};
+use cosmwasm_storage::{PrefixedStorage, ReadonlyPrefixedStorage};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{CanonicalAddr, Storage};
-use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+// === STORAGE PREFIXES ===
+pub const PREFIX_LOCKERS: &[u8] = b"lockers";
 
-pub static CONFIG_KEY: &[u8] = b"config";
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: CanonicalAddr,
+// === LOCKERS ===
+#[derive(Serialize, Debug, Deserialize, Clone, PartialEq, JsonSchema)]
+pub struct Locker {
+    pub password: String,
+    pub data: String,
 }
 
-pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
-    singleton(storage, CONFIG_KEY)
+pub struct Lockers<'a, S: Storage> {
+    storage: PrefixedStorage<'a, S>,
 }
 
-pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
-    singleton_read(storage, CONFIG_KEY)
+impl<'a, S: Storage> Lockers<'a, S> {
+    pub fn from_storage(storage: &'a mut S) -> Self {
+        Self {
+            storage: PrefixedStorage::new(PREFIX_LOCKERS, storage),
+        }
+    }
 }
+
+pub struct ReadonlyLockers<'a, S: ReadonlyStorage> {
+    storage: ReadonlyPrefixedStorage<'a, S>,
+}
+
+impl<'a, S: ReadonlyStorage> ReadonlyLockers<'a, S> {
+    pub fn from_storage(storage: &'a S) -> Self {
+        Self {
+            storage: ReadonlyPrefixedStorage::new(PREFIX_LOCKERS, storage),
+        }
+    }
+
+    fn as_readonly(&self) -> ReadonlyLockersImpl<ReadonlyPrefixedStorage<S>> {
+        ReadonlyLockersImpl(&self.storage)
+    }
+}
+
+/// This struct refactors out the readonly methods that we need for `Balances` and `ReadonlyBalances`
+/// in a way that is generic over their mutability.
+///
+/// This was the only way to prevent code duplication of these methods because of the way
+/// that `ReadonlyPrefixedStorage` and `PrefixedStorage` are implemented in `cosmwasm-std`
+struct ReadonlyLockersImpl<'a, S: ReadonlyStorage>(&'a S);
+
+impl<'a, S: ReadonlyStorage> ReadonlyLockersImpl<'a, S> {}
