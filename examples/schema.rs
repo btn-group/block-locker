9a823791edbf287f2fb85af53e0e6b19cbac25bc
Steven Chang <steven.chang@btn.group>
2021-10-21 14:46:12 +1100

    Add query to schema (#21)

--- examples/schema.rs
+++ examples/schema.rs
@@ -1,4 +1,4 @@
-use block_locker::msg::{HandleMsg, InitMsg, ReceiveAnswer, ReceiveMsg};
+use block_locker::msg::{HandleMsg, InitMsg, QueryAnswer, QueryMsg, ReceiveAnswer, ReceiveMsg};
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 use std::env::current_dir;
 use std::fs::create_dir_all;
@@ -10,6 +10,8 @@ fn main() {
     remove_schemas(&out_dir).unwrap();
     export_schema(&schema_for!(InitMsg), &out_dir);
     export_schema(&schema_for!(HandleMsg), &out_dir);
+    export_schema(&schema_for!(QueryAnswer), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(ReceiveAnswer), &out_dir);
     export_schema(&schema_for!(ReceiveMsg), &out_dir);
 }
